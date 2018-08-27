664df61cb592714e7c51a048fdf2a3f4d3dcd429
clarogira <clarogira@gmail.com>
2018-08-27 15:40:04 -0300

    w
    p

--- src/main/java/com/petapp/controller/AnimalController.java
+++ src/main/java/com/petapp/controller/AnimalController.java
@@ -1,17 +1,22 @@
 package com.petapp.controller;
 
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +27,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.petapp.controller.page.PageWrapper;
 import com.petapp.model.Animal;
+import com.petapp.model.Cliente;
 import com.petapp.model.Cor;
 import com.petapp.model.Raca;
 import com.petapp.repository.AnimalRepository;
@@ -92,5 +98,22 @@ public class AnimalController {
 		return mv;
 	}
 	
+	@RequestMapping(value = "/pesquisaAnimalRapido", method = RequestMethod.GET, consumes = { MediaType.APPLICATION_JSON_VALUE })
+	public @ResponseBody List<Animal> pesquisar(String nome) {
+		
+		
+		validarTamanhoNome(nome);
 	
+		return ar.findByNomeContainingIgnoreCase(nome);
+	}
+	private void validarTamanhoNome(String nome) {
+		if (StringUtils.isEmpty(nome) || nome.length() < 3) {
+			throw new IllegalArgumentException();
+		}
+	}
+	
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<Void> tratarIllegalArgumentException(IllegalArgumentException e) {
+		return ResponseEntity.badRequest().build();
+	}
 }
